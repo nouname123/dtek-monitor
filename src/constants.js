ea2b8ceb0405d475fe648f30bad642d42eb5a7f2
di.salim <136260552+nouname123@users.noreply.github.com>
2026-01-29 19:23:23 +0200

    Fix SHUTDOWNS_PAGE URL typo

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ import path from "node:path"
 export const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, CITY, STREET, HOUSE } =
   process.env
 
-export const SHUTDOWNS_PAGE = "https://www.dtek-krem.com.ua/ua/shutdowns"
+export const SHUTDOWNS_PAGE = "https://www.dtek-kem.com.ua/ua/shutdowns"
 
 export const LAST_MESSAGE_FILE = path.resolve("artifacts", `last-message.json`)
